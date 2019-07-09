91fe99827fef835e766e0fe326995c4c4a409a18
Vladislav Baranchuk <vbaranchuk.ctr@virtru.com>
2019-07-09 14:22:47 +0300

    Fix error state w/o message

--- stories/Input.js
+++ stories/Input.js
@@ -1,17 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, boolean } from '@storybook/addon-knobs';
+import { text, boolean, select } from '@storybook/addon-knobs';
 
 import { Input } from '../lib';
 
-storiesOf('Input', module).add('default', () => (
-  <div style={{ width: '400px', display: 'flex' }}>
-    <Input
-      error={text('Error message', '')}
-      info={text('Info message', '')}
-      value={text('Input value', 'Type here')}
-      disabled={boolean('Is disabled', false)}
-    />
-  </div>
-));
+storiesOf('Input', module).add('default', () => {
+  const errorTypes = ['boolean', 'string'];
+  const [defaultErrorType] = errorTypes;
+
+  const errorType = select('Error type', errorTypes, defaultErrorType);
+
+  return (
+    <div style={{ width: '400px', display: 'flex' }}>
+      <Input
+        error={errorType === 'boolean' ? boolean('Has error', false) : text('Error message', '')}
+        info={text('Info message', '')}
+        value={text('Input value', 'Type here')}
+        disabled={boolean('Is disabled', false)}
+      />
+    </div>
+  );
+});
