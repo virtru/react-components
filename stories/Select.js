5759cb788a38e45b38292a85ae0385dc2d36e7c3
Vladislav Baranchuk <vbaranchuk.ctr@virtru.com>
2019-07-10 14:59:37 +0300

    Finish select

--- stories/Select.js
+++ stories/Select.js
@@ -1,18 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { select, text, boolean } from '@storybook/addon-knobs';
+import { text, number, boolean } from '@storybook/addon-knobs';
 
 import { Select } from '../lib';
 
-storiesOf('Select', module).add('default', () => (
-  <div style={{ width: '400px', display: 'flex' }}>
-    <Select
-      placeholder={text('Placeholder', 'Select an Option')}
-      options={Array.from({ length: 5 }).map((_, index) => ({
-        label: `Menu Option`,
-        value: index,
-      }))}
-    />
-  </div>
-));
+storiesOf('Select', module).add('default', () => {
+  const optionsCount = number('Number of options', 5);
+  const optionPrefix = text('Option prefix', 'Menu Option');
+  const isShowIndex = boolean('Show option index', true);
+
+  return (
+    <div style={{ width: '400px', display: 'flex' }}>
+      <Select
+        placeholder={text('Placeholder', 'Select an Option')}
+        options={Array.from({ length: optionsCount }).map((_, index) => ({
+          label: `${optionPrefix} ${isShowIndex ? index + 1 : ''}`.trim(),
+          value: index,
+        }))}
+      />
+    </div>
+  );
+});
