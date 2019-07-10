e1b29fa7fa4cc3c549dd7e9a8df35bf5b00c16e8
Shaw Yu <syu@virtru.com>
2019-07-10 14:11:39 -0500

    fancier pipeline script

--- .buildkite/scripts/run-vrt.sh
+++ .buildkite/scripts/run-vrt.sh
@@ -6,6 +6,20 @@ source buildkite-scripts/utils/logging.sh
 
 buildkite-scripts/scripts/verify-node-modules.sh
 
-log-debug "Run visual regression tests"
+log-info "Run visual regression tests"
+
+set +e
 
 buildkite-scripts/mars/wrap/npm.sh run test:vrt
+
+EXIT_STATUS=$?
+
+if [[ ! -z "${EXIT_STATUS}" ]]; then
+  log-info "Uploading VRT difference images"
+
+  # unsetting BUILDKITE_ARTIFACT_UPLOAD_DESTINATION is temporary solution
+  # it changes upload destination from our private AWS bucket to Buildkite's one.
+  BUILDKITE_ARTIFACT_UPLOAD_DESTINATION="" buildkite-agent artifact upload ".loki/difference/*.png"
+fi
+
+exit $EXIT_STATUS
