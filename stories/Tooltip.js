bf89bbf5c34f327c1de59a3ff928c26e7a2f6cdf
Shaw Yu <syu@virtru.com>
2019-07-10 13:25:57 -0500

    prettier

--- stories/Tooltip.js
+++ stories/Tooltip.js
@@ -11,13 +11,12 @@ const [defaultPosition] = positions;
 const arrowPositions = Object.values(Tooltip.ARROW_POSITION);
 const [defaultArrowPosition] = arrowPositions;
 
-storiesOf('Tooltip', module)
-  .lokiSkip('default', () => (
-    <Tooltip
-      overlay={text('Overlay', '12/19/2016 10:42:00 PM')}
-      position={select('Position', positions, defaultPosition)}
-      arrowPosition={select('Arrow position', arrowPositions, defaultArrowPosition)}
-    >
-      <button type="button">Hover Me</button>
-    </Tooltip>
-  ));
+storiesOf('Tooltip', module).lokiSkip('default', () => (
+  <Tooltip
+    overlay={text('Overlay', '12/19/2016 10:42:00 PM')}
+    position={select('Position', positions, defaultPosition)}
+    arrowPosition={select('Arrow position', arrowPositions, defaultArrowPosition)}
+  >
+    <button type="button">Hover Me</button>
+  </Tooltip>
+));
