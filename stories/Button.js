d8aa83a11f59ece5c62e083748cc7b238b82d5ba
Shaw Yu <syu@virtru.com>
2019-08-01 13:11:46 -0500

    add button component and simple story

---
+++ stories/Button.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, boolean, select } from '@storybook/addon-knobs';
+
+import { Button } from '../lib';
+
+// eslint-disable-next-line react/prop-types
+const Container = ({ children }) => (
+  <div style={{ maxWidth: '400px', display: 'flex', flexGrow: '1' }}>{children}</div>
+);
+
+const variants = Object.values(Button.VARIANT);
+const sizes = Object.values(Button.SIZE);
+const defaultVariant = Button.VARIANT.SECONDARY;
+const defaultSize = Button.SIZE.MEDIUM;
+
+storiesOf('Button', module)
+  .lokiSkip('default', () => (
+    <Container>
+      <Button
+        variant={select('Variant', variants, defaultVariant)}
+        size={select('Size', sizes, defaultSize)}
+        fullWidth={boolean('Full Width')}
+      >
+        Label
+      </Button>
+    </Container>
+  ));
