ce2c170ee07ea1549b991b6024142822bbcc6659
Shaw Yu <syu@virtru.com>
2019-08-08 22:42:45 -0500

    fill out sizes for Text

--- stories/Text.js
+++ stories/Text.js
@@ -10,10 +10,15 @@ const Container = ({ children }) => (
   <div style={{ maxWidth: '575px' }}>{children}</div>
 );
 
+const sizes = Object.values(Text.SIZE);
+const defaultSize = Text.SIZE.NORMAL;
+
 storiesOf('Text', module)
   .lokiSkip('default', () => (
     <Container>
-      <Text>
+      <Text
+        size={select('Size', sizes, defaultSize)}
+      >
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac nunc ut lacus commodo efficitur. Sed ultrices eu enim in auctor. Nullam consectetur augue vitae metus ornare finibus. Sed blandit risus dignissim lacinia placerat. Nullam et risus porttitor nibh iaculis dignissim et quis elit.
       </Text>
     </Container>
