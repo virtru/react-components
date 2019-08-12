83c8f9b1eb4f956d570d98a884bf6096a6b53d6f
Shaw Yu <syu@virtru.com>
2019-08-12 08:29:56 -0500

    allow px in linter

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -11,8 +11,8 @@ module.exports = {
   rules: {
     'plugin/selector-tag-no-without-class': htmlTags,
     'declaration-property-unit-whitelist': {
-      'font-size': ['em', 'rem'],
-      'line-height': ['em', 'rem'],
+      'font-size': ['px', 'em', 'rem'],
+      'line-height': ['px', 'em', 'rem'],
     },
     'custom-property-empty-line-before': null,
   },
