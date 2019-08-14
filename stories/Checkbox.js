26999cefe760c43c0f0a28235ed5d9d1044d1b1a
Shaw Yu <syu@virtru.com>
2019-08-14 10:56:03 -0500

    set up stories

--- stories/Checkbox.js
+++ stories/Checkbox.js
@@ -1,21 +1,67 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { boolean, radios, text } from '@storybook/addon-knobs';
+import { boolean, select } from '@storybook/addon-knobs';
 import { Checkbox } from '../lib';
 
 const variants = Object.values(Checkbox.VARIANT);
 const [defaultVariant] = variants;
+const Container = ({ children }) => (
+  <div style={{ maxWidth: '400px', display: 'flex', flexGrow: '1' }}>{children}</div>
+);
+const printCheckboxTable = (variant) => {
+  const ref = useRef(null);
 
-storiesOf('Checkbox', module).add('default', () => (
-  <div style={{ textAlign: 'center' }}>
-    <Checkbox
-      isMobile={boolean('isMobile', false)}
-      disabled={boolean('disabled', false)}
-      variant={radios('variant', variants, defaultVariant)}
-      name={text('name', 'checkbox')}
-      onChange={() => {}}
-    />
-  </div>
-));
+  useLayoutEffect(() => {
+    ref.current.focus();
+  }, [ref]);
+
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Enabled</th>
+          <td>
+            <Checkbox
+              variant={variant}
+            />
+          </td>
+        </tr>
+        <tr>
+          <th>Disabled</th>
+          <td>
+            <Checkbox
+              variant={variant}
+              disabled
+            />
+          </td>
+        </tr>
+        <tr>
+          <th>Hover/Focus</th>
+          <td>
+            <Checkbox
+              variant={variant}
+              ref={ref}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+storiesOf('Checkbox', module)
+  .lokiSkip('default', () => (
+    <Container>
+      <Checkbox
+        disabled={boolean('Disabled', false)}
+        variant={select('Variant', variants, defaultVariant)}
+        onChange={() => {}}
+      />
+    </Container>
+  ))
+  .add('unchecked', () => printCheckboxTable(Checkbox.VARIANT.UNCHECKED))
+  .add('checked', () => printCheckboxTable(Checkbox.VARIANT.CHECKED))
+  .add('indeterminate', () => printCheckboxTable(Checkbox.VARIANT.INDETERMINATE))
+  .add('preset', () => printCheckboxTable(Checkbox.VARIANT.PRESET));
