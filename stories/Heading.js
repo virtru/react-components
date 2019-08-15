1f6006a7bfcd2c90bf0282db5bcfc973117fc5ff
Shaw Yu <syu@virtru.com>
2019-08-15 14:55:52 -0500

    refactor headings to use size and rank, fix lint

--- stories/Heading.js
+++ stories/Heading.js
@@ -1,32 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, boolean, select } from '@storybook/addon-knobs';
+import { text, select } from '@storybook/addon-knobs';
 
 import { Heading, Text } from '../lib';
 
 // eslint-disable-next-line react/prop-types
-const Container = ({ children }) => (
-  <div style={{ maxWidth: '575px' }}>{children}</div>
-);
+const Container = ({ children }) => <div style={{ maxWidth: '575px' }}>{children}</div>;
 
+const sizes = Object.values(Heading.SIZE);
 const ranks = [1, 2, 3, 4, 5, 6];
-const rankOverrides = [null].concat(ranks);
 
-storiesOf('Heading', module)
-  .lokiSkip('default', () => (
-    <Container>
-      <Text>
-        Text before
-      </Text>
-      <Heading
-        rank={select('Rank', ranks, 1)}
-        rankOverride={select('Rank Override', rankOverrides, null)}
-      >
-        {text('Heading text', 'Heading text')}
-      </Heading>
-      <Text>
-        Text after
-      </Text>
-    </Container>
-  ));
+storiesOf('Heading', module).lokiSkip('default', () => (
+  <Container>
+    <Text>Text before</Text>
+    <Heading size={select('Size', sizes, Heading.SIZE.XL)} rank={select('Rank', ranks, 1)}>
+      {text('Heading text', 'Heading text')}
+    </Heading>
+    <Text>Text after</Text>
+  </Container>
+));
--- stories/Text.js
+++ stories/Text.js
@@ -1,25 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, boolean, select } from '@storybook/addon-knobs';
+import { select } from '@storybook/addon-knobs';
 
 import { Text } from '../lib';
 
 // eslint-disable-next-line react/prop-types
-const Container = ({ children }) => (
-  <div style={{ maxWidth: '575px' }}>{children}</div>
-);
+const Container = ({ children }) => <div style={{ maxWidth: '575px' }}>{children}</div>;
 
 const sizes = Object.values(Text.SIZE);
 const defaultSize = Text.SIZE.NORMAL;
 
-storiesOf('Text', module)
-  .lokiSkip('default', () => (
-    <Container>
-      <Text
-        size={select('Size', sizes, defaultSize)}
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac nunc ut lacus commodo efficitur. Sed ultrices eu enim in auctor. Nullam consectetur augue vitae metus ornare finibus. Sed blandit risus dignissim lacinia placerat. Nullam et risus porttitor nibh iaculis dignissim et quis elit.
-      </Text>
-    </Container>
-  ));
+storiesOf('Text', module).lokiSkip('default', () => (
+  <Container>
+    <Text size={select('Size', sizes, defaultSize)}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac nunc ut lacus commodo
+      efficitur. Sed ultrices eu enim in auctor. Nullam consectetur augue vitae metus ornare
+      finibus. Sed blandit risus dignissim lacinia placerat. Nullam et risus porttitor nibh iaculis
+      dignissim et quis elit.
+    </Text>
+  </Container>
+));
