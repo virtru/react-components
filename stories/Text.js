e85fe34d33d25b4f819d5f88e829c3d7dd25879d
Shaw Yu <syu@virtru.com>
2019-08-15 15:10:17 -0500

    make text stories

--- stories/Text.js
+++ stories/Text.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { select } from '@storybook/addon-knobs';
+import { select, text } from '@storybook/addon-knobs';
 
 import { Text } from '../lib';
 
@@ -11,13 +11,49 @@ const Container = ({ children }) => <div style={{ maxWidth: '575px' }}>{children
 const sizes = Object.values(Text.SIZE);
 const defaultSize = Text.SIZE.NORMAL;
 
-storiesOf('Text', module).lokiSkip('default', () => (
-  <Container>
-    <Text size={select('Size', sizes, defaultSize)}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac nunc ut lacus commodo
-      efficitur. Sed ultrices eu enim in auctor. Nullam consectetur augue vitae metus ornare
-      finibus. Sed blandit risus dignissim lacinia placerat. Nullam et risus porttitor nibh iaculis
-      dignissim et quis elit.
-    </Text>
-  </Container>
-));
+const exampleText =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac nunc ut lacus commodo efficitur. Sed ultrices eu enim in auctor. Nullam consectetur augue vitae metus ornare finibus. Sed blandit risus dignissim lacinia placerat. Nullam et risus porttitor nibh iaculis dignissim et quis elit.';
+
+storiesOf('Text', module)
+  .lokiSkip('default', () => (
+    <Container>
+      <Text tag={text('Tag', 'p')} size={select('Size', sizes, defaultSize)}>
+        {text('Text', exampleText)}
+      </Text>
+      <Text tag={text('Tag', 'p')} size={select('Size', sizes, defaultSize)}>
+        {text('Text', exampleText)}
+      </Text>
+    </Container>
+  ))
+  .add('all sizes - p', () => (
+    <Container>
+      <Text tag="p" size={Text.SIZE.NORMAL}>
+        NORMAL - {exampleText}
+      </Text>
+      <Text tag="p" size={Text.SIZE.SMALL}>
+        SMALL - {exampleText}
+      </Text>
+      <Text tag="p" size={Text.SIZE.XSMALL}>
+        XSMALL - {exampleText}
+      </Text>
+      <Text tag="p" size={Text.SIZE.XXSMALL}>
+        XXSMALL - {exampleText}
+      </Text>
+    </Container>
+  ))
+  .add('all sizes - div', () => (
+    <Container>
+      <Text tag="div" size={Text.SIZE.NORMAL}>
+        NORMAL - {exampleText}
+      </Text>
+      <Text tag="div" size={Text.SIZE.SMALL}>
+        SMALL - {exampleText}
+      </Text>
+      <Text tag="div" size={Text.SIZE.XSMALL}>
+        XSMALL - {exampleText}
+      </Text>
+      <Text tag="div" size={Text.SIZE.XXSMALL}>
+        XXSMALL - {exampleText}
+      </Text>
+    </Container>
+  ));
