b03ec680809bf06978c736b925f6b285470ff8aa
Shaw Yu <syu@virtru.com>
2019-08-22 10:55:14 -0500

    copy node modules

--- .buildkite/scripts/build-and-deploy-storybook.sh
+++ .buildkite/scripts/build-and-deploy-storybook.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+source buildkite-scripts/utils/logging.sh
+
+buildkite-scripts/scripts/verify-node-modules.sh
+
 $(aws ecr get-login --no-include-email --region us-east-1)
+
 docker build -t storybook .
+
 docker tag storybook:latest 318258757195.dkr.ecr.us-east-1.amazonaws.com/storybook:latest
+
 docker push 318258757195.dkr.ecr.us-east-1.amazonaws.com/storybook:latest
+
 node scripts/deployStorybook.js
