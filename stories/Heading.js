214249b480c4f1508cd1ccd98e07d79f7f2618b0
Nathan Bakirci <nathan@virtru.com>
2020-03-13 13:28:20 -0400

    Add Heading color param

--- stories/Heading.js
+++ stories/Heading.js
@@ -10,11 +10,12 @@ const Container = ({ children }) => <div style={{ maxWidth: '575px' }}>{children
 
 const sizes = Object.values(Heading.SIZE);
 const ranks = [1, 2, 3, 4, 5, 6];
+const colors = ['black', 'red', 'blue', 'green'];
 
-const printHeadingExample = (size, rank) => (
+const printHeadingExample = (size, rank, color) => (
   <Container>
     <Text>Text before</Text>
-    <Heading size={size} rank={rank}>
+    <Heading size={size} rank={rank} color={color}>
       Heading text
     </Heading>
     <Text>Text after</Text>
@@ -25,7 +26,11 @@ storiesOf('Heading', module)
   .lokiSkip('default', () => (
     <Container>
       <Text>Text before</Text>
-      <Heading size={select('Size', sizes, Heading.SIZE.XLARGE)} rank={select('Rank', ranks, 1)}>
+      <Heading
+        size={select('Size', sizes, Heading.SIZE.XLARGE)}
+        rank={select('Rank', ranks, 1)}
+        color={select('Color', colors, 'black')}
+      >
         {text('Heading text', 'Heading text')}
       </Heading>
       <Text>Text after</Text>
@@ -36,4 +41,5 @@ storiesOf('Heading', module)
   .add('medium', () => printHeadingExample(Heading.SIZE.MEDIUM, 3))
   .add('small', () => printHeadingExample(Heading.SIZE.SMALL, 4))
   .add('xsmall', () => printHeadingExample(Heading.SIZE.XSMALL, 5))
-  .add('xxsmall', () => printHeadingExample(Heading.SIZE.XXSMALL, 6));
+  .add('xxsmall', () => printHeadingExample(Heading.SIZE.XXSMALL, 6))
+  .add('colored', () => printHeadingExample(Heading.SIZE.MEDIUM, 3, 'red'));
