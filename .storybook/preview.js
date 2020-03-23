03ce67e6ab16373d9a4bb8443a0376ef4acefa47
Shaw Yu <syu@virtru.com>
2020-03-23 17:06:23 -0400

    upgrade storybook and configs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  configure,
   addParameters,
   addDecorator,
 } from '@storybook/react';
@@ -10,7 +9,6 @@ import { themes } from '@storybook/theming';
 import 'virtru-typography';
 
 import styles from './styles.css';
-
 
 addParameters({
   options: {
@@ -25,13 +23,3 @@ addDecorator(Story => (
 ));
 
 addDecorator(withKnobs);
-
-function requireAll(requireContext) {
-  return requireContext.keys().map(requireContext);
-}
-
-function loadStories() {
-  requireAll(require.context('../stories', false, /\.js$/));
-}
-
-configure(loadStories, module);
