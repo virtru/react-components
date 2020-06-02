8035749408439072190a163ffa4287e8edc65055
Sergey Turchaninov <sturchaninov@lohika.com>
2020-06-02 15:59:33 +0300

    CORE-1074 initial commit

--- stories/Tooltip.js
+++ stories/Tooltip.js
@@ -10,13 +10,26 @@ const [defaultPosition] = positions;
 
 const arrowPositions = Object.values(Tooltip.ARROW_POSITION);
 const [defaultArrowPosition] = arrowPositions;
+const variants = Object.values(Tooltip.VARIANT);
+const [defaultVariant] = variants;
 
-storiesOf('Tooltip', module).lokiSkip('default', () => (
-  <Tooltip
-    overlay={text('Overlay', '12/19/2016 10:42:00 PM')}
-    position={select('Position', positions, defaultPosition)}
-    arrowPosition={select('Arrow position', arrowPositions, defaultArrowPosition)}
-  >
-    <button type="button">Hover Me</button>
-  </Tooltip>
-));
+storiesOf('Tooltip', module)
+  .lokiSkip('default', () => (
+    <Tooltip
+      overlay={text('Overlay', '12/19/2016 10:42:00 PM')}
+      position={select('Position', positions, defaultPosition)}
+      arrowPosition={select('Arrow position', arrowPositions, defaultArrowPosition)}
+    >
+      <button type="button">Hover Me</button>
+    </Tooltip>
+  ))
+  .add('light', () => (
+    <Tooltip
+      variant={select('Variant', variants, Tooltip.VARIANT.LIGHT)}
+      overlay={text('Overlay', '12/19/2016 10:42:00 PM')}
+      position={select('Position', positions, defaultPosition)}
+      arrowPosition={select('Arrow position', arrowPositions, defaultArrowPosition)}
+    >
+      <button type="button">Hover Me</button>
+    </Tooltip>
+  ));
