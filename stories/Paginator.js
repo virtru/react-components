9dd55af202009b70aa54fa9843f0a4f774d30e93
Vladislav Baranchuk <vbaranchuk.ctr@virtru.com>
2020-06-22 13:30:15 +0300

    Add paginator component

---
+++ stories/Paginator.js
@@ -0,0 +1,56 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { number, text } from '@storybook/addon-knobs';
+
+import { Paginator } from '../lib';
+
+storiesOf('Paginator', module)
+  .lokiSkip('default', () => (
+    <Paginator
+      min={number('Minimum items', 1)}
+      total={number('Total items', 89)}
+      start={number('Items start', 1)}
+      end={number('Items end', 25)}
+    >
+      {text('Text', '1 - 25  of  89')}
+    </Paginator>
+  ))
+  .add('without text', () => (
+    <Paginator
+      min={1}
+      total={10}
+      start={2}
+      end={3}
+    />
+  ))
+  .add('left disabled', () => (
+    <Paginator
+      min={1}
+      total={10}
+      start={1}
+      end={3}
+    >
+      Test
+    </Paginator>
+  ))
+  .add('right disabled', () => (
+    <Paginator
+      min={1}
+      total={10}
+      start={5}
+      end={10}
+    >
+      Test
+    </Paginator>
+  ))
+  .add('both disabled', () => (
+    <Paginator
+      min={1}
+      total={10}
+      start={1}
+      end={10}
+    >
+      Test
+    </Paginator>
+  ));
