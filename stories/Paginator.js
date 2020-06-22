9c62a68a964d27a6ca540ffaa5ffd632d9d54616
Vladislav Baranchuk <vbaranchuk.ctr@virtru.com>
2020-06-22 13:56:19 +0300

    Replace many different variants with single one

--- stories/Paginator.js
+++ stories/Paginator.js
@@ -5,6 +5,13 @@ import { number, text } from '@storybook/addon-knobs';
 
 import { Paginator } from '../lib';
 
+const renderVariant = (name, children) => (
+  <div style={{ marginBottom: 20 }}>
+    <h3>{name}</h3>
+    {children}
+  </div>
+);
+
 storiesOf('Paginator', module)
   .lokiSkip('default', () => (
     <Paginator
@@ -16,19 +23,26 @@ storiesOf('Paginator', module)
       {text('Text', '1 - 25  of  89')}
     </Paginator>
   ))
-  .add('without text', () => <Paginator min={1} total={10} start={2} end={3} />)
-  .add('left disabled', () => (
-    <Paginator min={1} total={10} start={1} end={3}>
-      Test
-    </Paginator>
-  ))
-  .add('right disabled', () => (
-    <Paginator min={1} total={10} start={5} end={10}>
-      Test
-    </Paginator>
-  ))
-  .add('both disabled', () => (
-    <Paginator min={1} total={10} start={1} end={10}>
-      Test
-    </Paginator>
+  .add('variants', () => (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      {renderVariant('Empty text', <Paginator total={10} start={2} end={3} />)}
+      {renderVariant(
+        'Left disabled',
+        <Paginator total={10} start={1} end={5}>
+          1 - 5 of 10
+        </Paginator>,
+      )}
+      {renderVariant(
+        'Right disabled',
+        <Paginator total={10} start={5} end={10}>
+          5 - 10 of 10
+        </Paginator>,
+      )}
+      {renderVariant(
+        'Both disabled',
+        <Paginator total={10} start={1} end={10}>
+          1 - 10 of 10
+        </Paginator>,
+      )}
+    </div>
   ));
